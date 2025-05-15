525c0fe02b10980f209144f31394a55a08fe933f
Pranesh Ragu <praneshragu@Praneshs-MacBook-Pro.local>
2025-05-15 10:56:49 +0530

    combine route features

--- HTTP_Router/router.go
+++ HTTP_Router/router.go
@@ -14,11 +14,28 @@ func ArticlesCategoryHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Category: %v\n", category)
 }
 
+func ProductDetailHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // Extracts path variables into a map
+	id := vars["id"]    // Gets the value of the "id" variable
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Product ID: %v\n", id)
+}
+
 func main() {
 	r := mux.NewRouter()
 
+	// Subrouter for product-related routes
+	productRouter := r.PathPrefix("/products/").Subrouter()
+
+	productRouter.HandleFunc("/{id}", ProductDetailHandler)
+
 	// Define a route with a named variable
-	r.HandleFunc("/articles/{category}", ArticlesCategoryHandler)
+	r.HandleFunc("/articles/{category}", ArticlesCategoryHandler).
+		Schemes("https").
+		Headers("X-Requested-With", "XMLHttpRequest").
+		Methods("GET", "POST").
+		Queries("type", "premium", "sort", "asc") //filter?type=premium&sort=asc
+
 	fmt.Println("Server started on http//localhost:8081")
 
 	http.ListenAndServe(":8081", r)
